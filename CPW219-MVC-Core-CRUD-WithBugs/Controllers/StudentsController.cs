185d466b7963f4ea422af07e3454bc052af9eb06
Logan Nelson <Lnelson0915@gmail.com>
2025-08-10 17:33:58 -0700

    Implement async database operations in StudentsController
    Updated the `StudentsController` to use asynchronous methods for saving changes to the database. This includes awaiting `_context.SaveChangesAsync()` after both updating and removing student records to ensure proper handling of database operations.

--- CPW219-MVC-Core-CRUD-WithBugs/Controllers/StudentsController.cs
+++ CPW219-MVC-Core-CRUD-WithBugs/Controllers/StudentsController.cs
@@ -113,6 +113,7 @@ public class StudentsController : Controller
             try
             {
                 _context.Update(student);
+                await _context.SaveChangesAsync();
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -157,6 +158,7 @@ public class StudentsController : Controller
         if (student != null)
         {
             _context.Student.Remove(student);
+            await _context.SaveChangesAsync();
         }
 
         return RedirectToAction(nameof(Index));
